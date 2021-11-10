916843af6c85762de702477763275d18f7b3cc5b
olga <oluciams84@gmail.com>
2021-11-10 16:47:01 -0500

    Adiciona estado y funcion para capturar el valor del input, para pasar el estado al p

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
 
-class App extends Component {
-  render() {
+function App (){
+
+  const [repeater, setRepeater] = useState();
+
+  const handleRepeater= (e)=>{
+   e.target.value  
+   setRepeater(e.target.value)
+  
+  }  
+  
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" value={repeater} onChange={handleRepeater} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{repeater}</p>
       </div>
     );
-  }
+  
 }
 
 export default App;
